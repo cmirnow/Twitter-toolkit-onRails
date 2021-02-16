710a1d8d80c5fb370a0239ac3af9ab6b0c629883
rishard <info@masterpro.ws>
2021-02-17 01:00:14 +0300

    html improvements

--- public/js/ajax.js
+++ public/js/ajax.js
@@ -9,7 +9,8 @@ function call() {
             $('#btnSubmit').prop('disabled', false);
             var div = document.createElement('div');
             div.innerHTML =
-                '<div class="alert alert-info" role="alert">' +
+                '<div class="alert alert-info alert-dismissible fade show" role="alert">' +
+                '<button type="button" class="close" data-dismiss="alert">&times;</button>' +
                 data.responseText +
                 '</div>';
             $('#flash-message').html(document.body.appendChild(div));
