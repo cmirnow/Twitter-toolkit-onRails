ad2a0383a66d6d43c58481d5433435bb6e008926
rishard <info@masterpro.ws>
2021-02-18 10:47:09 +0300

    bug fixes

--- app/jobs/follow_job.rb
+++ app/jobs/follow_job.rb
@@ -5,21 +5,21 @@ class FollowJob < ApplicationJob
     client = twi_client(args[0])
     list = follow(args[0])
     begin
-      list.take(100).reverse_each do |user|
+      list.take(100).each do |user|
         client.follow(user.id)
         list.delete(user)
         puts "follow: #{user.screen_name} #{Time.now}"
         sleep rand(30..60)
       end
+    rescue Twitter::Error::TooManyRequests,
+           Twitter::Error::Forbidden,
+           OpenSSL::SSL::SSLError,
+           Twitter::Error::ServiceUnavailable,
+           HTTP::Error
+      []
+      puts "rescue Twitter::Error #{Time.now}"
+      sleep 905
+      retry
     end
-  rescue Twitter::Error::TooManyRequests,
-         Twitter::Error::Forbidden,
-         OpenSSL::SSL::SSLError,
-         Twitter::Error::ServiceUnavailable,
-         HTTP::Error
-    []
-    puts "rescue Twitter::Error #{Time.now}"
-    sleep 905
-    retry
   end
 end
--- app/jobs/unfollow_job.rb
+++ app/jobs/unfollow_job.rb
@@ -5,21 +5,21 @@ class UnfollowJob < ApplicationJob
     client = twi_client(args[0])
     list = unfollow(args[0])
     begin
-      list.take(1000).reverse_each do |user|
+      list.take(1000).each do |user|
         client.unfollow(user.id)
         list.delete(user)
         puts "unfollow: #{user.screen_name} #{Time.now}"
         sleep rand(1..5)
       end
+    rescue Twitter::Error::TooManyRequests,
+           Twitter::Error::Forbidden,
+           OpenSSL::SSL::SSLError,
+           Twitter::Error::ServiceUnavailable,
+           HTTP::Error
+      []
+      puts "rescue Twitter::Error #{Time.now}"
+      sleep 90
+      retry
     end
-  rescue Twitter::Error::TooManyRequests,
-         Twitter::Error::Forbidden,
-         OpenSSL::SSL::SSLError,
-         Twitter::Error::ServiceUnavailable,
-         HTTP::Error
-    []
-    puts "rescue Twitter::Error #{Time.now}"
-    sleep 905
-    retry
   end
 end
