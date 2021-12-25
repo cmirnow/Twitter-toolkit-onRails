f5bee211e8585b984b50c6854244a3e51e94bc73
rishard <info@masterpro.ws>
2021-12-25 20:25:41 +0300

    mailer setting fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,6 +91,9 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.delivery_method = :smtp
+  # Specify the domain of your application here instead 'localhost:3000':
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
   :address              => "smtp.gmail.com",
